4c193b8aef712cd443ce06dbbac842856c2018de
U1410762 <u1410762@utah.edu>
2025-04-08 09:43:44 -0600

    Added background images

--- MorseCodeLearner/src/windows/mainwindow.cpp
+++ MorseCodeLearner/src/windows/mainwindow.cpp
@@ -13,6 +13,8 @@ MainWindow::MainWindow(QWidget *parent,
 {
     ui->setupUi(this);
 
+    this->setStyleSheet("QMainWindow { background-image: url(:/images/illustrated_background.jpg); background-position: center; width: 100%; height: 100%;}");
+
     stackedWidget = new QStackedWidget(this);
     setCentralWidget(stackedWidget);
 
@@ -28,7 +30,6 @@ MainWindow::MainWindow(QWidget *parent,
     QObject::connect(lessonWindow, &lessonwindow::goToLessonSelect, this, &MainWindow::onLearningNavClicked);
     QObject::connect(lessonSelectWindow, &lessonselectwindow::goHome, this, &MainWindow::goHome);
     QObject::connect(settingsWindow, &settingswindow::goHome, this, &MainWindow::goHome);
-
 }
 
 MainWindow::~MainWindow()
@@ -59,29 +60,35 @@ void MainWindow::onTranslatorNavClicked()
 {
     stackedWidget->setCurrentWidget(translatorWindow);
     translatorWindow->setUserOnThisPage(true);
+    this->setStyleSheet("QMainWindow { background-image: url(:/images/background.jpg); background-position: center; width: 100%; height: 100%;}");
 }
 
 void MainWindow::onPracticeNavClicked()
 {
     stackedWidget->setCurrentWidget(practiceWindow);
     practiceWindow->setUserOnThisPage(true);
+    this->setStyleSheet("QMainWindow { background-image: url(:/images/background.jpg); background-position: center; width: 100%; height: 100%;}");
 }
 
 void MainWindow::onLearningNavClicked()
 {
     stackedWidget->setCurrentWidget(lessonSelectWindow);
     lessonSelectWindow->setUserOnThisPage(true);
+    this->setStyleSheet("QMainWindow { background-image: url(:/images/background.jpg); background-position: center; width: 100%; height: 100%;}");
 }
 
 void MainWindow::onSettingsNavClicked()
 {
     stackedWidget->setCurrentWidget(settingsWindow);
     settingsWindow->setUserOnThisPage(true);
+    this->setStyleSheet("QMainWindow { background-image: url(:/images/background.jpg); background-position: center; width: 100%; height: 100%;}");
 }
 
 void MainWindow::goHome() {
     morseHandler->stopTimers();
     stackedWidget->setCurrentWidget(menuWindow);
     menuWindow->setUserOnThisPage(true);
+    this->setStyleSheet("QMainWindow { background-image: url(:/images/illustrated_background.jpg); background-position: center; width: 100%; height: 100%;}");
+
 }
 
