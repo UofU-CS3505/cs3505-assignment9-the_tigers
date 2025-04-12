89a6d371227f86b3a026dd5f3f146ce9b70f0d7f
U1410762 <u1410762@utah.edu>
2025-04-11 23:44:22 -0600

    Implemented lesson select page

--- MorseCodeLearner/include/windows/lessonselectwindow.h
+++ MorseCodeLearner/include/windows/lessonselectwindow.h
@@ -7,24 +7,52 @@ namespace Ui {
 class lessonselectwindow;
 }
 
+/**
+ * The lesson select page. Navigates between lessons and shows lesson completion.
+ *
+ * @name Aiden Fornalski and Chandler Eyre
+ * @date 04/11/2025
+ */
 class lessonselectwindow : public QWidget
 {
     Q_OBJECT
 
 public:
+    /**
+     * Constructor for a lesson select window.
+     */
     explicit lessonselectwindow(QWidget *parent = nullptr);
+
+    /**
+     * Destructor for a lesson select window.
+     */
     ~lessonselectwindow();
+
     void setUserOnThisPage(bool userOnThisPage);
+
     bool getUserOnThisPage();
+
+signals:
+    /**
+     * Signal that gets emitted when the back button is pressed.
+     */
+    void goHome();
+
+    /**
+     * Emitted when a lesson button is pressed.
+     */
+    void selectLesson(int lesson);
+
+private slots:
+    /**
+     * Resets the page's state when the back button is clicked.
+     */
+    void onBackButtonClicked();
 
 private:
     Ui::lessonselectwindow *ui;
-    bool userOnThisPage;
 
-signals:
-    void goHome();
-private slots:
-    void on_backButton_clicked();
+    bool userOnThisPage = false;
 };
 
 #endif // LESSONSELECTWINDOW_H
--- MorseCodeLearner/src/windows/lessonselectwindow.cpp
+++ MorseCodeLearner/src/windows/lessonselectwindow.cpp
@@ -9,7 +9,19 @@ lessonselectwindow::lessonselectwindow(QWidget *parent)
 
     ui->backButton->setIcon(QIcon(":/icons/back.svg"));
     ui->backButton->setIconSize(QSize(52, 52));
-    QObject::connect(ui->backButton, &QPushButton::clicked, this, &lessonselectwindow::on_backButton_clicked);
+    QObject::connect(ui->backButton, &QPushButton::clicked, this, &lessonselectwindow::onBackButtonClicked);
+
+    // Connections for lesson buttons
+    QObject::connect(ui->lessonButton_1, &QPushButton::clicked, this, [this](){emit selectLesson(1);});
+    QObject::connect(ui->lessonButton_2, &QPushButton::clicked, this, [this](){emit selectLesson(2);});
+    QObject::connect(ui->lessonButton_3, &QPushButton::clicked, this, [this](){emit selectLesson(3);});
+    QObject::connect(ui->lessonButton_4, &QPushButton::clicked, this, [this](){emit selectLesson(4);});
+    QObject::connect(ui->lessonButton_5, &QPushButton::clicked, this, [this](){emit selectLesson(5);});
+    QObject::connect(ui->lessonButton_6, &QPushButton::clicked, this, [this](){emit selectLesson(6);});
+    QObject::connect(ui->lessonButton_7, &QPushButton::clicked, this, [this](){emit selectLesson(7);});
+    QObject::connect(ui->lessonButton_8, &QPushButton::clicked, this, [this](){emit selectLesson(8);});
+    QObject::connect(ui->lessonButton_9, &QPushButton::clicked, this, [this](){emit selectLesson(9);});
+    QObject::connect(ui->lessonButton_10, &QPushButton::clicked, this, [this](){emit selectLesson(10);});
 }
 
 lessonselectwindow::~lessonselectwindow()
@@ -25,7 +37,7 @@ bool lessonselectwindow::getUserOnThisPage() {
     return userOnThisPage;
 }
 
-void lessonselectwindow::on_backButton_clicked()
+void lessonselectwindow::onBackButtonClicked()
 {
     emit goHome();
     userOnThisPage = false;
